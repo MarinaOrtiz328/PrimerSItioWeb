b219fac6a7f3ab368312cff7befe2ef0c1085e9a
Marina Ortiz <a329577@uach.mx>
2021-05-18 21:01:55 -0600

    Se agregan archivos app.js e index.html editados, base de los demas html, imagenes usadas, y el master.css para la pagina en general (faltan cambios aun)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs= require('fs');
 http.createServer((request, response)=>{
   console.log(request.url);
   const file = request.url=='/'?'./WWW/index.html': `./WWW${request.url}`;
+
   //const file2='./WWW/imagen.html';
       //"hola.como estas"=> split(".").pop()
         fs.readFile(file, (err,data)=>{
@@ -16,10 +17,33 @@ http.createServer((request, response)=>{
             response.write("not found");
             response.end();
           }else{
-            response.writeHead(404, {"Content.Type": "text/plain"});
+            const extension = file.split('.').pop();
+            switch (extension){
+              case 'txt':
+                response.writeHead(200, {"Content.Type": "text/plain"});
+                break;
+              case 'html':
+                response.writeHead(200, {"Content.Type": "text/html"});
+                break;
+              case 'jpeg':
+                response.writeHead(200, {"Content.Type": "image/jpeg"});
+                break;
+              case 'css':
+                response.writeHead(200, {"Content.Type": "text/css"});
+                break;
+              case 'js':
+                response.writeHead(200, {"Content.Type": "text/javascript"});
+                break;
+                case 'ico':
+                  response.writeHead(200, {"Content.Type": "image/x-icon"});
+                  break;
+              default:
+            }
+            response.writeHead(200, {"Content.Type": "text/plain"});
             response.write(data);
             response.end();
-            }
+          }
+
         });
 
 }).listen(8888);
