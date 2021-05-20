4b74ec7b0bb25715ed7583b73be5edcea30e86a0
Daniel Lopez <a315120@uach.mx>
2021-05-19 21:51:53 -0500

    Haciendo funcionar la forma

--- app.js
+++ app.js
@@ -11,39 +11,71 @@ http.createServer((request, response)=>{
 
   //const file2='./WWW/imagen.html';
       //"hola.como estas"=> split(".").pop()
-        fs.readFile(file, (err,data)=>{
-          if(err){
-            response.writeHead(404, {"Content.Type": "text/plain"});
-            response.write("not found");
-            response.end();
-          }else{
-            const extension = file.split('.').pop();
-            switch (extension){
-              case 'txt':
-                response.writeHead(200, {"Content.Type": "text/plain"});
-                break;
-              case 'html':
-                response.writeHead(200, {"Content.Type": "text/html"});
-                break;
-              case 'jpeg':
-                response.writeHead(200, {"Content.Type": "image/jpeg"});
-                break;
-              case 'css':
-                response.writeHead(200, {"Content.Type": "text/css"});
-                break;
-              case 'js':
-                response.writeHead(200, {"Content.Type": "text/javascript"});
-                break;
-                case 'ico':
-                  response.writeHead(200, {"Content.Type": "image/x-icon"});
-                  break;
-              default:
-            }
-            response.writeHead(200, {"Content.Type": "text/plain"});
-            response.write(data);
-            response.end();
-          }
 
+      if(request.url == '/form'){
+        let data = [];
+        
+        request.on("data", value => {
+            data.push(value);
+        }).on("end", () => {
+            let params = Buffer.concat(data).toString();
+            params += '\n';
+
+            fs.appendFile("data.txt", params, (error) => {
+                if (error){
+                    response.writeHead(500,{"Content-Type":"text/plain"});
+                    response.write("Error en el servidor asjdasjdasj");
+                    response.end();
+                }
+            });
+
+            console.log(params);
         });
+
+        fs.readFile("./WWW/index.html", (error, html) => {
+            if (error){
+                response.writeHead(500,{"Content-Type":"text/plain"});
+                response.write("Error en el servidor wea fome");
+                response.end();
+            }
+            response.writeHead(200, {"Content-Type":"text/html"});
+            response.end(html);
+        });
+      } else {
+          fs.readFile(file, (err,data)=>{
+            if(err){
+              response.writeHead(404, {"Content.Type": "text/plain"});
+              response.write("not found");
+              response.end();
+            }else{
+              const extension = file.split('.').pop();
+              switch (extension){
+                case 'txt':
+                  response.writeHead(200, {"Content.Type": "text/plain"});
+                  break;
+                case 'html':
+                  response.writeHead(200, {"Content.Type": "text/html"});
+                  break;
+                case 'jpeg':
+                  response.writeHead(200, {"Content.Type": "image/jpeg"});
+                  break;
+                case 'css':
+                  response.writeHead(200, {"Content.Type": "text/css"});
+                  break;
+                case 'js':
+                  response.writeHead(200, {"Content.Type": "text/javascript"});
+                  break;
+                  case 'ico':
+                    response.writeHead(200, {"Content.Type": "image/x-icon"});
+                    break;
+                default:
+              }
+              response.writeHead(200, {"Content.Type": "text/plain"});
+              response.write(data);
+              response.end();
+            }
+
+          });
+      }
 
 }).listen(8888);
